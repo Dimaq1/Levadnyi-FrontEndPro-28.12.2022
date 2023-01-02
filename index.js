c5859405dc590350132b3811065d604f1bf1f920
Dimaq1 <levadni1717@gmail.com>
2023-01-02 12:09:15 +0200

    Homework3

---
+++ index.js
@@ -0,0 +1,5 @@
+const userName = prompt("What is your name?");
+const userAge = prompt("How old are you?");
+const userCity = prompt("Which city are you from?");
+alert(`Hello, ${userName}! You are ${userAge} years old. You live in the city of ${userCity}.`)
+console.log(`Hello, ${userName}! You are ${userAge} years old. You live in the city of ${userCity}.`)
