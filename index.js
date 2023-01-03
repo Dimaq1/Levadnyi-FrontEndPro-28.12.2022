09c8a64599ab73d44aa1298d141900a9187cba39
Dimaq1 <levadni1717@gmail.com>
2023-01-03 13:53:07 +0200

    Homework4

---
+++ index.js
@@ -0,0 +1,21 @@
+const userName = prompt("Як тебе звати?");
+const userAge = +prompt("Скільки тобі років?");
+const smoke = confirm("Ти куриш?");
+
+if (userAge > 18) {
+  if (smoke) {
+    alert(`Привіт ${userName}, що ж, ти сам обрав цей шлях..`);
+    console.log(`Привіт ${userName}, що ж, ти сам обрав цей шлях..`);
+  } else {
+    alert(`Привіт ${userName}, чудово! Ти мабуть ще й спортом займаєшся!`);
+    console.log(`Привіт ${userName}, чудово! Ти мабуть ще й спортом займаєшся!`);
+  }
+} else if (userAge < 18) {
+  if (smoke) {
+    alert(`Привіт ${userName}! А батьки знають про це?`);
+    console.log(`Привіт ${userName}! А батьки знають про це?`);
+  } else {
+    alert(`Привіт ${userName}! Правильно, зараз головне добре вчитися!`);
+    console.log(`Привіт ${userName}! Правильно, зараз головне добре вчитися!`);
+  }
+}
