7dcd4e79dd1567866b3cd571d225e5ee17f556b0
Dimaq1 <levadni1717@gmail.com>
2023-01-03 18:06:41 +0200

    minor change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const userName = prompt("Як тебе звати?");
 const userAge = +prompt("Скільки тобі років?");
 const smoke = confirm("Ти куриш?");
 
-if (userAge > 18) {
+if (userAge => 18) {
   if (smoke) {
     alert(`Привіт ${userName}, що ж, ти сам обрав цей шлях..`);
     console.log(`Привіт ${userName}, що ж, ти сам обрав цей шлях..`);
@@ -10,7 +10,7 @@ if (userAge > 18) {
     alert(`Привіт ${userName}, чудово! Ти мабуть ще й спортом займаєшся!`);
     console.log(`Привіт ${userName}, чудово! Ти мабуть ще й спортом займаєшся!`);
   }
-} else if (userAge < 18) {
+} else {
   if (smoke) {
     alert(`Привіт ${userName}! А батьки знають про це?`);
     console.log(`Привіт ${userName}! А батьки знають про це?`);
