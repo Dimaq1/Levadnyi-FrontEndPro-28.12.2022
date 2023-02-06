f53234120fad6ad5e9b36fe3faaec4fdf464603d
Dimaq1 <levadni1717@gmail.com>
2023-02-06 16:38:46 +0200

    HW11

---
+++ index.js
@@ -0,0 +1,11 @@
+function pow(num, degree) {
+  debugger;
+  if (degree === 1) {
+    return num;
+  } else {
+    return num * pow(num, degree - 1)
+  }
+}
+
+const result = pow(3, 3)
+console.log(result);
