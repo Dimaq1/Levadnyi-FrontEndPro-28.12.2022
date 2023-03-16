31e379244bce7a9c9729422164e1720e50843550
Dimaq1 <levadni1717@gmail.com>
2023-03-16 18:10:00 +0200

    HW15

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const button1 = document.querySelector('.btn1')
 const button2 = document.querySelector('.btn2')
 
 
-let div = document.createElement('div')
-div.innerHTML = prompt('Text:')
-div.className = 'text'
 
 button1.addEventListener('click', function () {
+  let div = document.createElement('div')
+  div.innerHTML = prompt('Text:')
+  div.className = 'text'
   wrap.appendChild(div)
 })
 
