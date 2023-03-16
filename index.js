0b7d5405d0c2f4205c467285edb6ae2669e35b74
Dimaq1 <levadni1717@gmail.com>
2023-03-16 17:41:13 +0200

    HW15

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const wrap = document.querySelector('.wrap')
 const button1 = document.querySelector('.btn1')
 const button2 = document.querySelector('.btn2')
 
+
 let div = document.createElement('div')
-div.innerHTML = prompt('Text:')
+div.innerHTML = prompt('Text1:')
 div.className = 'text'
 
 button1.addEventListener('click', function () {
-  wrap.append(div)
+  wrap.appendChild(div)
+  wrap.appendChild(div2)
+  wrap.appendChild(div3)
+
 })
 
 button2.addEventListener('click', function () {
-  wrap.removeChild(div)
+  if (wrap.lastChild !== button1 && wrap.lastChild !== button2) {
+    wrap.removeChild(wrap.lastChild)
+  }
 })
 
 div.addEventListener('click', function () {
